43e060970349175c207dfd712f106f6eaf15fce9
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-02-24 14:57:56 +0200

    Add tests

--- 217-Contains-Duplicate.js
+++ 217-Contains-Duplicate.js
@@ -1,12 +1,10 @@
-var containsDuplicate = function(nums){
-    seen = new Set
-    for(let num of nums){
-        if (seen.has(num)) return true
-        seen.add(num)
-    }
-    return false
-}
-
-
+var containsDuplicate = function (nums) {
+  seen = new Set();
+  for (let num of nums) {
+    if (seen.has(num)) return true;
+    seen.add(num);
+  }
+  return false;
+};
 
 module.exports = containsDuplicate;
--- 217-Contains-Duplicate.test.js
+++ 217-Contains-Duplicate.test.js
@@ -1,16 +1,22 @@
 // app.test.js
-const containsDuplicate = require('./217-Contains-Duplicate'); // Adjust the path to where your function is exported
+const containsDuplicate = require("./217-Contains-Duplicate"); // Adjust the path to where your function is exported
 
-describe('containsDuplicate function', () => {
-  test('returns true for array containing duplicates', () => {
-    expect(containsDuplicate([1,2,3,1])).toBe(true);
+describe("containsDuplicate function", () => {
+  test("returns true for array containing duplicates", () => {
+    expect(containsDuplicate([1, 2, 3, 1])).toBe(true);
   });
 
-  test('returns false for array without duplicates', () => {
-    expect(containsDuplicate([1,2,3,4])).toBe(false);
+  test("returns true for array containing duplicates", () => {
+    expect(containsDuplicate([1, 2, 3, -1, 4, 5, 6, 7, 8, 9, 11, 13, -1])).toBe(
+      true
+    );
   });
 
-  test('returns false for empty array', () => {
+  test("returns false for array without duplicates", () => {
+    expect(containsDuplicate([1, 2, 3, 4])).toBe(false);
+  });
+
+  test("returns false for empty array", () => {
     expect(containsDuplicate([])).toBe(false);
   });
 });
