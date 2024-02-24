b79a406da36bd1fd04a5e0414a3b9ae1c1c9f148
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-02-24 23:48:08 +0200

    Add tests

--- 217-Contains-Duplicate.test.js
+++ 217-Contains-Duplicate.test.js
@@ -16,6 +16,10 @@ describe("containsDuplicate function", () => {
     expect(containsDuplicate([1, 2, 3, 4])).toBe(false);
   });
 
+  test("returns false for array without duplicates", () => {
+    expect(containsDuplicate([1, 0.1, 0.01, 0.001, 0.0001])).toBe(false);
+  });
+
   test("returns false for empty array", () => {
     expect(containsDuplicate([])).toBe(false);
   });
