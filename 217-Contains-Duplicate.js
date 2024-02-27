d6ca61b67155a8d52d143064129ad9443231215c
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-02-27 12:02:03 +0200

    Add solution using Hash Table

--- 217-Contains-Duplicate.js
+++ 217-Contains-Duplicate.js
@@ -1,10 +1,24 @@
+// Set Data Structure
+
+// var containsDuplicate = function (nums) {
+//   seen = new Set();
+//   for (let num of nums) {
+//     if (seen.has(num)) return true;
+//     seen.add(num);
+//   }
+//   return false;
+// };
+
 var containsDuplicate = function (nums) {
-  seen = new Set();
-  for (let num of nums) {
-    if (seen.has(num)) return true;
-    seen.add(num);
+  myHash = {};
+  for (num of nums) {
+    if (myHash[num] === undefined) {
+      myHash[num] = 1;
+    } else return true;
   }
   return false;
+  //   console.log(myHash);
 };
 
+console.log(containsDuplicate([1, 2, 3, 8]));
 module.exports = containsDuplicate;
